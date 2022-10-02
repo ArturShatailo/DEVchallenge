7a955ae07e7321e65f1b21d7af55440444c934bc
Arthur <arturshatailo@gmail.com>
2022-10-03 02:53:26 +0300

    PersonServiceBean commented

--- src/main/java/online/task/devchallenge/service/PersonServiceBean.java
+++ src/main/java/online/task/devchallenge/service/PersonServiceBean.java
@@ -11,23 +11,27 @@ import java.util.Map;
 
 @AllArgsConstructor
 @Service
-public class PersonServiceBean {
+public class PersonServiceBean implements PersonCrudService{
 
     private final PersonRepository personRepository;
 
+    @Override
     public Person create(Person person) {
         return personRepository.save(person);
     }
 
+    @Override
     public Person getPersonById(String id) {
         return personRepository.findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("Person not found with id = " + id));
     }
 
+    @Override
     public List<Person> gerAllPersons(){
         return personRepository.findAll();
     }
 
+    @Override
     public Map<String, Integer> updateConnections(String id, Map<String, Integer> trust_connections) {
 
         Person person = getPersonById(id);
@@ -40,17 +44,7 @@ public class PersonServiceBean {
         return checkConnectionsAdded(person, trust_connections);
     }
 
-    private Map<String, Integer> checkConnectionsAdded(Person person, Map<String, Integer> trust_connections) {
-
-        Map<String, Integer> returnConnectionsAdded = new HashMap<>();
-
-        for (String key : trust_connections.keySet())
-            if (person.getConnections().containsKey(key))
-                returnConnectionsAdded.put(key, trust_connections.get(key));
-
-        return returnConnectionsAdded;
-    }
-
+    @Override
     public Map<String, Integer> deleteConnections(String id, Map<String, Integer> trust_connections) {
 
         Person person = getPersonById(id);
@@ -63,5 +57,23 @@ public class PersonServiceBean {
 
         return trust_connections;
     }
+
+     /*
+     * The method will check if connections received as a Map are included in Person's connections.
+     * In other words, method to check if connections were added.
+     * Returns a Map of connections that were added.
+     */
+    private Map<String, Integer> checkConnectionsAdded(Person person, Map<String, Integer> trust_connections) {
+
+        Map<String, Integer> returnConnectionsAdded = new HashMap<>();
+
+        for (String key : trust_connections.keySet())
+            if (person.getConnections().containsKey(key))
+                returnConnectionsAdded.put(key, trust_connections.get(key));
+
+        return returnConnectionsAdded;
+    }
+
+
 
 }
